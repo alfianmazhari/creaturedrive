2daa770ba977594bb899a12c98c2ccbaee4093a1
alfianmazhari <63595088+alfianmazhari@users.noreply.github.com>
2023-02-09 12:20:59 +0800

    Add files via upload

---
+++ Assets/Network/NetworkManager.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NetworkManager : MonoBehaviour {
+	const string VERSION = "v0.0.1";
+	public string RoomName = "CD";
+
+	public string[] CharacterName;
+	
+	public GameObject[] characters;
+	public GameObject SelectedCharacter;
+	public int SelectedCharacterIndex = 0;
+	
+	public GameObject ImageTarget;
+	
+	public Transform P1SpawnPoint;
+	public Transform P2SpawnPoint;
+	
+	public Vector3 CharacterScale;
+
+	private CharacterHealth CH;
+	
+	void Start () 
+	{
+		PhotonNetwork.ConnectUsingSettings(VERSION);
+		SelectedCharacterIndex = PlayerPrefs.GetInt ("SelectedCharacter");
+
+		if(SelectedCharacterIndex == 0) 
+		{
+			CharacterScale.x = 100;
+			CharacterScale.y = 100;
+			CharacterScale.z = 100;
+		}
+		else if(SelectedCharacterIndex == 1) 
+		{
+			CharacterScale.x = 80;
+			CharacterScale.y = 80;
+			CharacterScale.z = 80;
+		}
+		CharacterName[0] = "Dragon";
+		CharacterName[1] = "Lava Golem";
+	}
+
+	void Update ()
+	{
+	}
+
+	void OnJoinedLobby ()
+	{
+		RoomOptions roomOptions = new RoomOptions() { isVisible = false, maxPlayers = 2 };
+		PhotonNetwork.JoinOrCreateRoom(RoomName, roomOptions, TypedLobby.Default);
+	}
+	
+	void OnJoinedRoom ()
+	{
+
+		if (PhotonNetwork.countOfPlayersInRooms <= 0) 
+		{
+			SelectedCharacter = (GameObject) PhotonNetwork.Instantiate (CharacterName [SelectedCharacterIndex], P1SpawnPoint.localPosition, P1SpawnPoint.localRotation, 0);
+		}
+		else if (PhotonNetwork.countOfPlayersInRooms >= 1) 
+		{
+			SelectedCharacter = (GameObject) PhotonNetwork.Instantiate (CharacterName [SelectedCharacterIndex], P2SpawnPoint.localPosition, P2SpawnPoint.localRotation, 0);
+		}
+
+		SelectedCharacter.transform.SetParent(ImageTarget.transform);
+		SelectedCharacter.GetComponent<MeleeSystem> ().enabled = true;
+		SelectedCharacter.GetComponent<CharacterHealth> ().enabled = true;
+		SelectedCharacter.GetComponent<AccelerometerMovement> ().enabled = true;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Network/NetworkPlayer.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkPlayer : Photon.MonoBehaviour {
+
+	Vector3 RealPosition = Vector3.zero;
+	Quaternion RealRotation = Quaternion.identity;
+
+	Animator Anim;
+
+	void Start () 
+	{
+		Anim = GetComponent<Animator> ();
+		if (Anim == null) 
+		{
+			Debug.LogError ("There is no Animator on this character prefab!");
+		}
+	}
+
+	void Update()
+	{
+		if (photonView.isMine) 
+		{
+		} 
+		else 
+		{
+			transform.position = Vector3.Lerp(transform.position, RealPosition, 0.1f);
+			transform.rotation = Quaternion.Lerp(transform.rotation, RealRotation, 0.1f);
+		}
+	}
+
+	public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		if (stream.isWriting) 
+		{
+			stream.SendNext (transform.position);
+			stream.SendNext (transform.rotation);
+
+			stream.SendNext (Anim.GetBool("Run"));
+			stream.SendNext (Anim.GetBool("Attack"));
+			stream.SendNext (Anim.GetBool("Drop"));
+			stream.SendNext (Anim.GetBool("PowerAttack"));
+			stream.SendNext (Anim.GetBool("SpecialAttack"));
+		} 
+		else 
+		{
+			RealPosition = (Vector3) stream.ReceiveNext();
+			RealRotation = (Quaternion) stream.ReceiveNext();
+
+			this.Anim.SetBool("Run", (bool)stream.ReceiveNext());
+			this.Anim.SetBool("Attack", (bool)stream.ReceiveNext());
+			this.Anim.SetBool("Drop", (bool)stream.ReceiveNext());
+			this.Anim.SetBool("PowerAttack", (bool)stream.ReceiveNext());
+			this.Anim.SetBool("SpecialAttack", (bool)stream.ReceiveNext());
+		}
+	}
+}
